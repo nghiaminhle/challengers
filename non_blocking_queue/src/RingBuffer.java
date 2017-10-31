9dfecd64f03d18caf0ecff161fabbc82f3c3e2a3
nghiaminhle <admin@LAP00332s-MacBook-Pro.local>
2017-10-31 22:53:57 +0700

    ring buffer nonblocking

--- non_blocking_queue/src/RingBuffer.java
+++ non_blocking_queue/src/RingBuffer.java
@@ -3,10 +3,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class RingBuffer {
 	private int size;
-	private AtomicInteger head = new AtomicInteger(-1);
-	private AtomicInteger tail = new AtomicInteger(-1);
+	private int head = 0;
+	private int tail = 0;
+	private int count = 0;
 	private Object[] items;
-	private AtomicInteger[] atomicItems;
+	private AtomicInteger flag = new AtomicInteger(0);
 
 	public RingBuffer() {
 		this(100);
@@ -15,34 +16,42 @@ public class RingBuffer {
 	public RingBuffer(int size) {
 		this.size = size;
 		this.items = new Object[this.size];
-		this.atomicItems = new AtomicInteger[this.size];
-		for (int i = 0; i < this.size; i++) {
-			this.atomicItems[i] = new AtomicInteger(0);
-		}
 	}
 
-	public boolean enqueue(Object item) {
-		int h = this.head.get();
-		int v = this.atomicItems[(h + 1) % this.size].get();
-		if (v == 0 && this.atomicItems[(h + 1) % this.size].compareAndSet(v, v + 1)) {
-			this.items[(h + 1) % this.size] = item;
-			while (!this.head.compareAndSet(h, (h + 1) % this.size)) {
-				h = this.head.get();
+	public Boolean enqueue(Object item) {
+		int f = this.flag.get();
+		while (true && this.count < this.size) {
+			if (this.count == this.size)
+				return false;
+			if (f == 0 && this.flag.compareAndSet(f, f + 1)) {
+				if (this.count == this.size)
+					return false;
+				this.items[this.head] = item;
+				this.head = (this.head + 1) % this.size;
+				this.count++;
+				f = this.flag.get();
+				this.flag.compareAndSet(f, f - 1);
+				return true;
 			}
-			return true;
+			f = this.flag.get();
 		}
 		return false;
 	}
 
 	public Object dequeue() {
-		int t = this.tail.get();
-		int v = this.atomicItems[(t + 1) % this.size].get();
-		if (v == 1 && this.atomicItems[(t + 1) % this.size].compareAndSet(v, v - 1)) {
-			Object item = this.items[(t + 1) % this.size];
-			while (!this.tail.compareAndSet(t, (t + 1) % this.size)) {
-				t = this.tail.get();
+		int f = this.flag.get();
+		while (true && this.count > 0) {
+			if (this.count == 0)
+				return null;
+			if (f == 0 && this.flag.compareAndSet(f, f + 1)) {
+				Object item = this.items[this.tail];
+				this.tail = (this.tail + 1) % this.size;
+				this.count--;
+				f = this.flag.get();
+				this.flag.compareAndSet(f, f - 1);
+				return item;
 			}
-			return item;
+			f = this.flag.get();
 		}
 		return null;
 	}
